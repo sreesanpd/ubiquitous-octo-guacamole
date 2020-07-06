cd07f5eae5c564533737b81d9e0020c265690cbf
sreesanpd <sreesanpd@users.noreply.github.com>
2020-07-07 01:55:14 +0200

    Update google-filestore-backup.sh

--- google-filestore-backup.sh
+++ google-filestore-backup.sh
@@ -26,7 +26,7 @@ mount $FILESTORE_IP_SECONDARY:/$FILESHARE_NAME_SECONDARY /mnt/$FILESHARE_MOUNT_S
 
 rsync -avz /mnt/$FILESHARE_MOUNT_PRIMARY/ /mnt/$FILESHARE_MOUNT_SECONDARY/
 
-#sleep for few minutes
+#sleep for few minutes ##
 
 sleep 300
 
